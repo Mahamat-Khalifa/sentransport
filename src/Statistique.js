d073e64dc2975891366eb8405ede708d7699f69d
Mahamat Khalifa <alibechirmahamatkhalifa@esp.sn>
2026-04-30 20:26:37 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

---
+++ src/Statistique.js
@@ -0,0 +1,13 @@
+// src/Statistique.js
+import './Statistique.css';
+
+function Statistique() {
+  return (
+    <div className="statistique">
+      <span className="stat-chiffre">10</span>
+      <span className="stat-libelle">lignes</span>
+    </div>
+  );
+}
+
+export default Statistique;
