19f81b3967d4c69dd65e4a3f921952319175f8e5
Mahamat Khalifa <alibechirmahamatkhalifa@esp.sn>
2026-05-02 13:37:47 +0000

    Lab3 : etat, recherche  et selection de lignes

--- src/ListeLignes.js
+++ src/ListeLignes.js
@@ -2,16 +2,12 @@ import LigneBus from './LigneBus';
 import './ListeLignes.css';
 
 function ListeLignes({ lignes }) {
-
   return (
     <div className="liste-lignes">
-
       <h2 className="liste-titre">Lignes Dakar Dem Dikk</h2>
-
       <p className="liste-description">
         {lignes.length} lignes disponibles
       </p>
-
       {lignes.map(ligne => (
         <LigneBus
           key={ligne.id}
@@ -19,12 +15,11 @@ function ListeLignes({ lignes }) {
           depart={ligne.depart}
           arrivee={ligne.arrivee}
           arrets={ligne.arrets}
+          couleur={ligne.couleur}
         />
       ))}
-
     </div>
   );
-
 }
 
 export default ListeLignes;
